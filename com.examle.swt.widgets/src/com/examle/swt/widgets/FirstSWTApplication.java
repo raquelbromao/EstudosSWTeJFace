b3ea9eaa4a1d979e9f5622a270d7df0e9d228f06
raquelbromao <raquelbromao@gmail.com>
2016-07-20 14:35:13 -0300

    Adiciona comandos para dividir a janela em duas, cada uma com um texto e uma cor de fundo diferente.

--- com.examle.swt.widgets/src/com/examle/swt/widgets/FirstSWTApplication.java
+++ com.examle.swt.widgets/src/com/examle/swt/widgets/FirstSWTApplication.java
@@ -1,8 +1,11 @@
 package com.examle.swt.widgets;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class FirstSWTApplication {
 
@@ -16,6 +19,20 @@ public class FirstSWTApplication {
         shell.setLayout(new FillLayout());
         
         //TODO add some widgets to the Shell
+     // Shell can be used as container
+        Label label = new Label(shell, SWT.BORDER);
+        label.setText("This is a label:");
+        label.setToolTipText("This is the tooltip of this label");
+
+        Text text = new Text(shell, SWT.NONE);
+        text.setText("This is the text in the text widget");
+        text.setBackground(display.getSystemColor(SWT.COLOR_BLACK));
+        text.setForeground(display.getSystemColor(SWT.COLOR_WHITE));
+
+        // set widgets size to their preferred size
+        text.pack();
+        label.pack(); 
+        
         shell.open();
         while (!shell.isDisposed()) {
             if (!display.readAndDispatch())
